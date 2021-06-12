9a9cbc9cb7873640f9f00a07434432e48914b8c0
EC2 Default User <ec2-user@ip-172-31-79-9.ec2.internal>
2021-06-12 17:59:26 +0000

    first commit

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  devise_for :users
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update]
 end
---
+++ bookers2-debug/db/migrate/20210612170004_relations2.rb
@@ -0,0 +1,4 @@
+class Relations2 < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
